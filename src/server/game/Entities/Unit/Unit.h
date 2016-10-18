a9240bfbf25188331bdad62e1bf322c382423a65
Bodeguero <joaocarlos.jc.souza@gmail.com>
2016-10-18 08:18:52 -0300

    Core/Spells: Implemented Spells Druid
    * Spell Dru Tooth and Claw Absorb
    * Spell Dru Natures Cure
    * Spell Dru Wild Mushroom Heal
    * Spell Dru Wild Mushroom Growing
    * Spell Dru Wild Mushroom Resto
    * Spell Dru Swiftmend Heal
    * Spell Dru Genesis
    * Spell Dru Yseras Gift
    * Spell Druid Glyph of Charm Woodland Creature
    * Spell Dru Heart Wild
    * Spell Dru Dream of Cenarius
    * Spell Dru Dream of Cenarius Restor
    * Spell Dru Force of Nature
    * Spell Dru Pet Force of Nature Heal
    * Spell Dru treants Spells Calculation
    * Spell Druid Natures Grace
    * Update Spell Dru  Alignment to Spell Dru Celestial Alignment

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -1,10 +1,11 @@
 /*
- * Copyright (C) 2008-2015 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2014 MaNGOS <http://getmangos.com/>
+ * Copyright (C) 2013-2016 JadeCore <https://www.jadecore.tk/>
+ * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2011-2016 Project SkyFire <http://www.projectskyfire.org/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 3 of the License, or (at your
+ * Free Software Foundation; either version 2 of the License, or (at your
  * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
@@ -2312,6 +2313,7 @@ class Unit : public WorldObject
 
 		int32 GetEclipsePower() { return _eclipsePower; };
 		void SetEclipsePower(int32 power, bool send = true);
+		void SendEclipse();
 
 		uint32 GetHealingDoneInPastSecs(uint32 secs);
         uint32 GetHealingTakenInPastSecs(uint32 secs);
