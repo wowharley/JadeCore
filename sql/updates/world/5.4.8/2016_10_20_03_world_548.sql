efcea61b28e9ca5e7e444b81500bfe2486b6627d
Bodeguero <joaocarlos.jc.souza@gmail.com>
2016-10-20 11:14:15 -0300

    DB/Misc: Working for Siege Of Ogrimmar - Part 02

---
+++ sql/updates/world/5.4.8/2016_10_20_03_world_548.sql
@@ -0,0 +1,5 @@
+-- Working for Siege Of Ogrimmar
+-- By Bodeguero for JadeCore.
+
+UPDATE `creature_template` SET `faction_A`='14', `faction_H`='14', `InhabitType`='1' WHERE (`entry`='69534');
+UPDATE `creature_template` SET `faction_A`='14', `faction_H`='14', `InhabitType`='1' WHERE (`entry`='69510');
