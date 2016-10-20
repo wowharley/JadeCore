1718976f3aa5e9b0a2a95f8e76e13e5e3e2456b4
Bodeguero <joaocarlos.jc.souza@gmail.com>
2016-10-20 11:40:28 -0300

    DB/Misc: Working for Goblin Starting Zone - Part 01

---
+++ sql/updates/world/5.4.8/2016_10_20_05_world.sql
@@ -0,0 +1,32 @@
+-- Working for Goblin Zone
+-- By Bodeguero for JadeCore.
+
+UPDATE `creature_template` SET `ScriptName`='npc_fourth_and_goal_target' WHERE `entry`='37203';
+UPDATE `creature_template` SET `ScriptName`='npc_defiant_troll' WHERE `entry`='34830';
+UPDATE `creature_template` SET `ScriptName`='npc_bling_bling' WHERE `entry`='35120';
+UPDATE `creature_template` SET `ScriptName`='npc_bling_bling' WHERE `entry`='35126';
+UPDATE `creature_template` SET `ScriptName`='npc_bling_bling' WHERE `entry`='35128';
+UPDATE `creature_template` SET `ScriptName`='npc_bling_bling' WHERE `entry`='35130';
+UPDATE `creature_template` SET `ScriptName`='npc_bambo' WHERE `entry`='35186';
+UPDATE `creature_template` SET `ScriptName`='npc_shark_gob' WHERE `entry`='37114';
+UPDATE `creature_template` SET `ScriptName`='npc_boucanier_gob' WHERE `entry`='37179';
+UPDATE `creature_template` SET `ScriptName`='npc_citoyen_gob' WHERE `entry`='35075';
+UPDATE `creature_template` SET `ScriptName`='npc_citoyen_gob' WHERE `entry`='35234';
+UPDATE `creature_template` SET `ScriptName`='npc_robot_gob' WHERE `entry`='37598';
+UPDATE `creature_template` SET `ScriptName`='npc_gw' WHERE `entry`='37106';
+UPDATE `creature_template` SET `ScriptName`='npc_gl_gob' WHERE `entry`='35486';
+UPDATE `creature_template` SET `ScriptName`='npc_original_hot_rod' WHERE `entry`='34840';
+UPDATE `creature_template` SET `ScriptName`='npc_original_hot_rod' WHERE `entry`='37676';
+UPDATE `creature_template` SET `ScriptName`='npc_friend_gob' WHERE `entry`='34957';
+UPDATE `creature_template` SET `ScriptName`='npc_friend_gob' WHERE `entry`='34958';
+UPDATE `creature_template` SET `ScriptName`='npc_chipie_quest_giver_end_event' WHERE `entry`='37680';
+UPDATE `gameobject_template` SET `ScriptName`='gob_bank_gobelin' WHERE `entry`='195525';
+UPDATE `gameobject_template` SET `ScriptName`='gob_canon_gobelin' WHERE `entry`='207355';
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('66298', 'spell_klaxon');
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('66299', 'spell_radio');
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('67495', 'spell_bank_67495');
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('67496', 'spell_bank_67496');
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('67497', 'spell_bank_67497');
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('67498', 'spell_bank_67498');
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('67682', 'spell_kabummbomb');
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('67499', 'spell_bank_67499');
