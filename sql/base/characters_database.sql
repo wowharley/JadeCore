0a760f05dd57a45718c76f8424d7a7aba290cdde
wowharley <devrevolution2016@gmail.com>
2016-10-19 21:50:33 -0400

    Updated Characters Base

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -271,7 +271,6 @@ CREATE TABLE `calendar_events` (
 `dungeon`  int(10) NOT NULL DEFAULT '-1' ,
 `eventtime`  int(10) UNSIGNED NOT NULL DEFAULT 0 ,
 `flags`  int(10) UNSIGNED NOT NULL DEFAULT 0 ,
-`time2`  int(10) UNSIGNED NOT NULL DEFAULT 0 ,
 PRIMARY KEY (`id`)
 )
 ENGINE=InnoDB
