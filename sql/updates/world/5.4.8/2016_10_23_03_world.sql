2d3f8fcd0be8ad214c48c28067de9c423bf713f8
Bodeguero <joaocarlos.jc.souza@gmail.com>
2016-10-23 19:50:48 -0300

    Quick Fix

--- sql/updates/world/5.4.8/2016_10_23_03_world.sql
+++ sql/updates/world/5.4.8/2016_10_23_03_world.sql
@@ -1,4 +1,4 @@
 -- Update JCDB-548
 -- By Bodeguero for JadeCore.
 
-REPLACE INTO `version` VALUES ('JadeCore rev. (Archieved) (Win32, Release)', 'Archieved', 'JCDB 548.08', '08');
+REPLACE INTO `version` VALUES ('JadeCore revision   : bc03ca082e8c+ 2016-10-23 17:34:14 -0500 (5.4.8 branch)', 'Archieved', 'JCDB 548.08', '08');
