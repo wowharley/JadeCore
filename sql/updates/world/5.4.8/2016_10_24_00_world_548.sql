91ced89cecedd2487ba2a6a5c4a251fe65b57f2d
cybrann <trulex8@gmail.com>
2016-10-24 19:15:48 +0200

    DB/Misc - Update 2016_10_24_00_world_548.sql

--- sql/updates/world/5.4.8/2016_10_24_00_world_548.sql
+++ sql/updates/world/5.4.8/2016_10_24_00_world_548.sql
@@ -7,14 +7,17 @@ UPDATE `creature_template` SET `ScriptName` = 'mob_sha_pool' WHERE `entry` = 715
 UPDATE `creature_template` SET `ScriptName` = 'mob_swirl_target' WHERE `entry` = 71550;
 UPDATE `creature_template` SET `ScriptName` = 'mob_swirl_zone' WHERE `entry` = 71548;
 
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_seeping_sha';
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_seeping_sha_damage';
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_split';
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_swirl';
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_swirl_damage';
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_sha_bolt_missile';
 
-INSERT INTO `spell_script_names` VALUES (143281, 'spell_seeping_sha_damage'),
+INSERT INTO `spell_script_names` VALUES (143281, 'spell_seeping_sha'),
+(143286, 'spell_seeping_sha_damage'),
 (143020, 'spell_split'),
 (143309, 'spell_swirl'),
 (143412, 'spell_swirl_damage'),
+(143293, 'spell_sha_bolt_damage'),
 (129067, 'spell_sha_bolt_missile');
