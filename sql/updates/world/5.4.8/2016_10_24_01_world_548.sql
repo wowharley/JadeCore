91dd30a6d527c887fd503f32c9eb80037c305d71
phulo <yoandis2006@gmail.com>
2016-10-24 10:20:06 -0400

    Duplicates Spirit Healers Entry

---
+++ sql/updates/world/5.4.8/2016_10_24_01_world_548.sql
@@ -0,0 +1,5 @@
+-- Duplicates Spirit Healers
+
+DELETE FROM `creature` WHERE `guid` IN (3232, 332192);
+DELETE FROM `creature` WHERE `guid` IN (3241,332189);
+DELETE FROM `creature` WHERE `guid` IN (332181,3238);
